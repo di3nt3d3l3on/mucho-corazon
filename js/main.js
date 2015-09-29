999d6c787149f365e4107aa43c57b6cd2631e2d8
unknown <curso@MBL08.stktrainning.com>
2015-09-29 11:41:24 -0500

    Addded js main

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function ($){
+    
+$(document).ready(function () {
+    $('button').on('click', function (e) {
+        var ripple = $(this).find('.ripple');
+        ripple.removeClass('animate');
+        var x = parseInt(e.pageX - $(this).offset().left) - (ripple.width() / 2);
+        var y = parseInt(e.pageY - $(this).offset().top) - (ripple.height() / 2);
+        ripple.css({top: y, left: x}).addClass('animate');
+    });
+});
+
+})(jQuery)
